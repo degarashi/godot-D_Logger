c127acc450a3d0263a872852db54fc98753dea2f
Degarashi <degarasu@gmail.com>
2026-03-13 16:30:11 +0900

    [update] Adjustment of log formatting logic
    Refactoring of the _format_log function to include the message directly within the formatted string. Removal of redundant message printing in individual logging methods to ensure a cleaner and more consistent output structure.

--- addons/d_logger/d_logger_full.gd
+++ addons/d_logger/d_logger_full.gd
@@ -3,16 +3,26 @@ class_name DLoggerFull
 extends DLoggerBase
 
 
-func _format_log(_msg: Variant, category: String, level: String) -> String:
+func _format_log(msg: Variant, category: String, level: String) -> String:
 	# 秒単位に変換（1234ms -> 1.234s）
 	var seconds := Time.get_ticks_msec() / 1000.0
 
 	var cat_str := ""
 	if category != "":
-		cat_str = "[%s] " % category
+		cat_str = category
 
 	# 全体で7文字分、小数点は3桁
-	return "[%7.3fs] %s %s%s [%s]" % [seconds, prefix, cat_str, _get_caller_info(), level]
+	return (
+		"[%7.3fs][%s]%s %s - [%s] %s"
+		% [
+			seconds,
+			prefix,
+			_get_caller_info(),
+			cat_str,
+			level,
+			msg,
+		]
+	)
 
 
 func _get_caller_info() -> String:
@@ -24,20 +34,20 @@ func _get_caller_info() -> String:
 
 
 func debug(msg: Variant, category: String = "") -> void:
-	print_rich("[color=gray]%s[/color] %s" % [_format_log(msg, category, "DEBUG"), str(msg)])
+	print_rich("[color=gray]%s[/color]" % [_format_log(msg, category, "DEBUG")])
 
 
 func info(msg: Variant, category: String = "") -> void:
-	print_rich("[b][color=cyan]%s[/color][/b] %s" % [_format_log(msg, category, "INFO"), str(msg)])
+	print_rich("[b][color=cyan]%s[/color][/b]" % [_format_log(msg, category, "INFO")])
 
 
 func warn(msg: Variant, category: String = "") -> void:
 	var header := _format_log(msg, category, "WARN")
-	print_rich("[b][color=yellow]%s[/color][/b] %s" % [header, str(msg)])
+	print_rich("[b][color=yellow]%s[/color][/b]" % [header])
 	push_warning("%s %s" % [header, str(msg)])
 
 
 func error(msg: Variant, category: String = "") -> void:
 	var header := _format_log(msg, category, "ERROR")
-	print_rich("[b][color=red]%s[/color][/b] %s" % [header, str(msg)])
+	print_rich("[b][color=red]%s[/color][/b]" % [header])
 	push_error("%s %s" % [header, str(msg)])
