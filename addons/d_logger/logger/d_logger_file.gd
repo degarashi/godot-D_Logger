673da48b403aa04dee7b1807ceabd4f3b0957f09
Degarashi <degarasu@gmail.com>
2026-03-29 16:44:13 +0900

    [update] Optimization of file logging in DLoggerFile
    Refactoring of the file logger to keep the file handle open instead of repeatedly opening and closing it.

--- addons/d_logger/logger/d_logger_file.gd
+++ addons/d_logger/logger/d_logger_file.gd
@@ -2,40 +2,50 @@
 extends RefCounted
 
 # ------------- [Constants] -------------
-const _C := preload("uid://cwfe01280qmo7")
+const _C = preload("uid://cwfe01280qmo7")
 
 # ------------- [Private Variable] -------------
 var _file_path: String
+var _file: FileAccess
 
 
 # ------------- [Callbacks] -------------
 func _init(path: String) -> void:
 	assert(DLoggerFunc.is_logger(self))
-
 	_file_path = path
-	# Instead of WRITE, just create if it doesn't exist
+
+	# Check directory existence (automatically created if it does not exist)
+	var dir_path := _file_path.get_base_dir()
+	if not dir_path.is_empty() and not DirAccess.dir_exists_absolute(dir_path):
+		DirAccess.make_dir_recursive_absolute(dir_path)
+
+	# Keep the file open in append mode (READ_WRITE)
+	# If the file does not exist, create it with WRITE and close it immediately
 	if not FileAccess.file_exists(_file_path):
 		var f := FileAccess.open(_file_path, FileAccess.WRITE)
 		if f:
-			f.store_line("--- Log Created: %s ---" % Time.get_datetime_string_from_system())
+			var init_msg := "--- Log Created: {0} ---"
+			f.store_line(init_msg.format([Time.get_datetime_string_from_system()]))
+			f.close()
 
-	# Append the session separator line on startup using READ_WRITE
-	_write_line("=== New Session Started: %s ===" % Time.get_datetime_string_from_system())
+	# Open again and seek to end
+	_file = FileAccess.open(_file_path, FileAccess.READ_WRITE)
+
+	if _file:
+		_file.seek_end()
+		# Write session separator line
+		var session_msg := "=== New Session Started: {0} ==="
+		_write_line(session_msg.format([Time.get_datetime_string_from_system()]))
+	else:
+		# Safety fallback if the file could not be opened
+		var error_msg := "DLoggerFile: Failed to open file for appending: {0}"
+		push_error(error_msg.format([_file_path]))
 
 
 # ------------- [Private Method] -------------
 func _write_line(line: String) -> void:
-	# Open with READ_WRITE, seek to end and write
-	# If this fails, first check if this mode is supported
-	var f := FileAccess.open(_file_path, FileAccess.READ_WRITE)
-	if f:
-		f.seek_end()
-		f.store_line(line)
-		f.flush()
-	else:
-		# As a fallback if READ_WRITE fails (e.g., permissions)
-		# If opening and writing each time is too heavy, consider a design that holds FileAccess
-		push_error("DLoggerFile: Failed to open file for appending: " + _file_path)
+	if _file:
+		_file.store_line(line)
 
 
 # ------------- [Public Method] -------------
