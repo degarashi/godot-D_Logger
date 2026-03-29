ccdfe69bd2e5dbcf1ed2da75b58aedb8d35a8cc7
Degarashi <degarasu@gmail.com>
2026-03-29 13:37:57 +0900

    [add] Creation of find_logger_from_ancestor in common.gd
    Addition of a static function to traverse scene tree hierarchy toward parents.
    Search of children at each ancestor level to find a node implementing the logger interface.

--- addons/d_logger/common.gd
+++ addons/d_logger/common.gd
@@ -24,3 +24,17 @@ static func is_logger(logger: Object) -> bool:
 			return false
 
 	return true
+
+
+## Searches for a specific interface in the parent direction from the specified node
+## @param start_node The node to start the search from
+## @return The child object implementing the logger interface if found, null otherwise
+static func find_logger_from_ancestor(start_node: Node) -> Object:
+	var current := start_node
+	while current:
+		for child: Node in current.get_children():
+			if is_logger(child):
+				return child
+		current = current.get_parent()
+
+	return null
