3b85ec58cf8b0a2127975f64b65823d373fd668d
Degarashi <degarasu@gmail.com>
2026-03-30 22:01:14 +0900

    [fix] Prevention of logger initialization racing issues
    Separation of project settings fallback logic and static method extraction for cleaner object instantiation.

--- addons/d_logger/d_logger_node.gd
+++ addons/d_logger/d_logger_node.gd
@@ -10,25 +10,25 @@ extends DLoggerNodeBase
 func _enter_tree() -> void:
 	if not ProjectSettings.settings_changed.is_connected(_on_settings_changed):
 		ProjectSettings.settings_changed.connect(_on_settings_changed)
-	_on_settings_changed()
 
 
 func _ready() -> void:
+	_logger = _create_logger_from_settings(_init_param if _init_param else DLoggerInitParam.new())
 	if not _init_param:
-		_init_param = DLoggerInitParam.new()
-	_create_logger()
+		_on_settings_changed()
+
+
+# ------------- [Private Static Method] -------------
+static func _create_logger_from_settings(param: DLoggerInitParam) -> DLoggerClass:
+	return DLoggerClass.new(
+		param.prefix_override if not param.prefix_override.is_empty() else null,
+		param.min_level_override,
+		param.console_enabled_override,
+		param.file_path_override
+	)
 
 
 # ------------- [Private Method] -------------
 func _on_settings_changed() -> void:
-	if _logger:
+	if not _init_param and _logger:
 		_logger.setup_logger()
-
-
-func _create_logger() -> void:
-	_logger = DLoggerClass.new(
-		_init_param.prefix_override if not _init_param.prefix_override.is_empty() else null,
-		_init_param.min_level_override,
-		_init_param.console_enabled_override,
-		_init_param.file_path_override
-	)
