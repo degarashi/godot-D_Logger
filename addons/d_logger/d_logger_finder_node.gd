39131bec6bcc399b052ca35caa240679c24ca7df
Degarashi <degarasu@gmail.com>
2026-03-30 14:59:42 +0900

    [add] Addition of on_log_found signal to DLoggerFinder
    Introduction of a custom signal that triggers when a valid logger is detected.

--- addons/d_logger/d_logger_finder_node.gd
+++ addons/d_logger/d_logger_finder_node.gd
@@ -2,9 +2,14 @@
 class_name DLoggerFinder
 extends DLoggerNodeBase
 
+signal on_log_found(logger: Object)
+
 
 # ------------- [Callbacks] -------------
 func _ready() -> void:
 	var logger := DLoggerFunc.find_logger_from_ancestor(self)
 	if logger:
 		_logger = logger.get_logger()
+
+	if _logger:
+		on_log_found.emit(_logger)
