dcf99acdf3f977de06b63824102eb404bd238762
Degarashi <degarasu@gmail.com>
2026-03-30 14:32:53 +0900

    [add] Addition of get_logger function to common.gd
    Refactoring of logger validation logic and addition of a helper method to safely retrieve DLoggerClass instances from nodes.

--- addons/d_logger/common.gd
+++ addons/d_logger/common.gd
@@ -2,13 +2,10 @@ class_name DLoggerFunc
 extends Object
 
 
-## Checks if the given object meets the requirements of a logger
+## @brief Checks if the given object meets the requirements of a logger interface
 ## @param logger The object to be checked
 ## @return True if it implements all required methods, false otherwise
-static func is_logger(logger: Object) -> bool:
-	if not logger:
-		return false
-
+static func has_logger_interface(logger: Object) -> bool:
 	const REQUIRED_METHODS: PackedStringArray = [
 		"is_debug_enabled",
 		"is_info_enabled",
@@ -19,12 +16,37 @@ static func is_logger(logger: Object) -> bool:
 		"warn",
 		"error"
 	]
-
 	for method_name: String in REQUIRED_METHODS:
 		if not logger.has_method(method_name):
 			return false
 
 	return true
+
+
+## @brief Retrieves a logger from a node or an object
+## @param logger The object to attempt to get the logger from
+## @return The child object implementing the logger interface if found, null otherwise
+static func get_logger(logger: Object) -> Object:
+	if not logger:
+		return null
+	if has_logger_interface(logger):
+		return logger
+
+	if logger is Node:
+		if logger.has_method("get_logger"):
+			logger = logger.get_logger()
+
+		if not logger:
+			return null
+
+	return null
+
+
+## @brief Validates if the object is an active logger
+## @param logger The object to be checked
+## @return True if it is a valid logger, false otherwise
+static func is_logger(logger: Object) -> bool:
+	return get_logger(logger) != null
 
 
 ## Searches for a specific interface in the parent direction from the specified node
