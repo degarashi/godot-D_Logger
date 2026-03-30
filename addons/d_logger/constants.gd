0e939185fff31a65b737f5281ce53068347af62b
Degarashi <degarasu@gmail.com>
2026-03-30 22:04:22 +0900

    [update] Add class_name to constants.gd

--- addons/d_logger/constants.gd
+++ addons/d_logger/constants.gd
@@ -1,5 +1,6 @@
 @tool
 ## Constant definitions used throughout D-Logger
+class_name DLoggerConstants
 extends Object
 
 enum LogLevel { NOT_SPECIFIED = -1, DEBUG = 0, INFO = 1, WARN = 2, ERROR = 3 }
