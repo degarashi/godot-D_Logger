04eb64a888680e222c0654cc4a65d8419e811da4
Degarashi <degarasu@gmail.com>
2026-06-23 17:53:58 +0900

    [fix] guard against get_logger() crash on DLoggerClass

--- addons/d_logger/d_logger_finder_node.gd
+++ addons/d_logger/d_logger_finder_node.gd
@@ -9,7 +9,7 @@ signal on_log_found(logger: Object)
 func _ready() -> void:
 	var logger := DLoggerFunc.find_logger_from_ancestor(self)
 	if logger:
-		_logger = logger.get_logger()
+		_logger = logger.get_logger() if logger.has_method(&"get_logger") else logger
 
 	if _logger:
 		on_log_found.emit(_logger)
